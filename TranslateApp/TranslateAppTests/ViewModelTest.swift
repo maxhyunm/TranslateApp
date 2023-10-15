fe1ff1dd714b9eeefd5c571a06cf055a1bc77803
Min Hyun <minnimida@gmail.com>
2023-10-15 15:58:49 +0900

    feat: ViewModelTest 생성

--- TranslateApp/TranslateAppTests/ViewModelTest.swift
+++ TranslateApp/TranslateAppTests/ViewModelTest.swift
@@ -6,30 +6,33 @@
 //
 
 import XCTest
+import RxSwift
+import RxCocoa
+import RxBlocking
+@testable import TranslateApp
 
 final class ViewModelTest: XCTestCase {
+    var sut: MainViewModel!
+    let repository = TranslatorRepository(networkManager: NetworkManager())
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        sut = MainViewModel(repository: repository)
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func test_스캔된_텍스트가_정상적으로_input에_저장된다() {
+        // given
+        let expectation = "input"
+        
+        // when
+        sut.scanText(expectation)
+        let result = sut.inputText
+        
+        // then
+        XCTAssertEqual(expectation, result)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
